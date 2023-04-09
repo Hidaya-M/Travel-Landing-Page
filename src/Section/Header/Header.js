7ae50001553f080d1b4eada1025e42be7718ba69
Hidaya-M <doyimegateli@gmail.com>
2023-04-09 16:37:02 +0200

    first commit

--- src/Section/Header/Header.js
+++ src/Section/Header/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./Header.css"
+
 import Logo from '../../Components/Logo/Logo'
+
 
 export default function Header() {
   return (
-    <div>
+   
+    <>
      <div class="navbar navbar-expand-md bg-dark navbar-dark text-white fixed-top">
         <div class="container">
           <Logo />
@@ -13,8 +16,8 @@ export default function Header() {
             </button>
             <div class="navbar-collapse collapse" id="mainmenu" >
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a href="#hero" class="nav-link">Home</a></li>
-                    <li class="nav-item"><a href="#features" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="/#" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="/services" class="nav-link">Services</a></li>
                     <li class="nav-item dropdown">
                         <a href="n" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Products</a>
                         <ul class="dropdown-menu">
@@ -28,6 +31,8 @@ export default function Header() {
             </div>
         </div>
     </div>
-    </div>
+
+    </>
+  
   )
 }
